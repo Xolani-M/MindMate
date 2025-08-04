b2ea5296fa787d5f7f4cef52a8dd569bfe8edd70
Xolani <nathanielmvana@gmail.com>
2025-08-04 15:13:53 +0200

    Add SeekerId property to ChatRequestDto for personalized chatbot requests

--- aspnet-core/src/MINDMATE.Application/Chatbot/Dto/ChatRequestDto.cs
+++ aspnet-core/src/MINDMATE.Application/Chatbot/Dto/ChatRequestDto.cs
@@ -1,7 +1,9 @@
+using System;
 namespace MINDMATE.Application.Chatbot.Dto
 {
     public class ChatRequestDto
     {
         public string Message { get; set; }
+        public Guid SeekerId { get; set; }
     }
 }
